c4ffecfd44b45cc926ccb4de7843489a3238e041
liuyang <liuyang8218@163.com>
2017-05-18 21:59:38 +0800

    初次提交

---
+++ src/main/java/com/chronos/mssm/dao/IUserDao.java
@@ -0,0 +1,46 @@
+package com.chronos.mssm.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.chronos.mssm.model.User;
+
+public interface IUserDao {
+
+	/**
+	 * 添加用户
+	 * 
+	 * @param user
+	 */
+	int addUser(User user);
+	
+	/**
+	 * 查询用户
+	 * 
+	 * @param username
+	 */
+	void queryUserByName(String username);
+	/**
+	 * 查询用户列表
+	 * 
+	 * @param nickName
+	 * @return
+	 */
+	List queryUserList(String nickName);
+	/**
+	 * 删除用户
+	 * 
+	 * @param user
+	 * @return
+	 */
+	int deleteUser(User user);
+	/**
+	 * 更新用户
+	 * 
+	 * @Date 2017年5月18日 下午5:52:15
+	 * @param user
+	 * @return
+	 */
+	int updateUser(User user);
+}
---
+++ src/main/java/com/chronos/mssm/dao/impl/UserDaoImpl.java
@@ -0,0 +1,77 @@
+package com.chronos.mssm.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.chronos.mssm.dao.IUserDao;
+import com.chronos.mssm.model.User;
+/**
+ * 用户dao层实现
+ * 
+ * @author chronos
+ * @date 2017年5月12日 下午4:19:16
+ */
+@Repository
+public class UserDaoImpl implements IUserDao{
+
+	@Autowired
+	private SqlSession sqlSession;
+	//mapper文件的namespace
+	private String mapperNamespace = "com.chronos.mssm.mapper.user";
+
+	
+	public SqlSession getSqlSession() {
+		return sqlSession;
+	}
+
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	/**
+	 * 添加一个用户
+	 */
+	@Override
+	public int addUser(User user) {
+		int i =this.sqlSession.insert(mapperNamespace+".insertUser",user);
+		return i;
+	}
+	/**
+	 * 查询用户
+	 */
+	@Override
+	public void queryUserByName(String username) {
+		
+		User  user = this.sqlSession.selectOne(mapperNamespace+".selectUser", username);
+		
+	}
+
+	@Override
+	public int deleteUser(User user) {
+		// TODO Auto-generated method stub
+		int i = this.sqlSession.delete(mapperNamespace+".deleteUser", user);
+		return i;
+	}
+
+	@Override
+	public int updateUser(User user) {
+		// TODO Auto-generated method stub
+		int i = this.sqlSession.update(mapperNamespace+".updateUser", user);
+		return i;
+	}
+
+	@Override
+	public List queryUserList(String nickName) {
+		// TODO Auto-generated method stub
+		List list = this.sqlSession.selectList(this.mapperNamespace+".selectUserList",nickName);
+		return list;
+	}
+	
+	
+	
+	
+}
